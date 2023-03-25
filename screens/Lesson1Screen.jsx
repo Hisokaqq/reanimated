327fc8428ca43392639c6a536d312d4aec774803
Alex <baklan4ik1993@gmail.com>
2023-03-25 15:03:21 +0100

    Lesson1

--- screens/Lesson1Screen.jsx
+++ screens/Lesson1Screen.jsx
@@ -1,20 +1,40 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Animated, {
     useSharedValue,
     withTiming,
     useAnimatedStyle,
     Easing,
+    withSpring,
+    withRepeat,
   } from 'react-native-reanimated';
+import { SafeAreaView } from 'react-native';
 
 const Lesson1Screen = () => {
+
+  const progress = useSharedValue(0)
+  const scale = useSharedValue(0)
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+      transform: [{scale: scale.value}, {rotate: `${Math.PI * 2 * progress.value}rad`}],
+      borderRadius: progress.value*20,
+      
+    }
+  })
+
+  useEffect(() => {
+    progress.value = withRepeat(withSpring(1), -1, true)
+    scale.value = withRepeat(withSpring(1), -1, true)
+    
+  }, [])
+
   return (
-    <View>
-      <Text>Lesson1Screen</Text>
-    </View>
+    <SafeAreaView style={{ alignItems: "center", justifyContent: "center", flex:1 }}>
+      <Animated.View style={[{ height: 100, width: 100, backgroundColor: "black" }, reanimatedStyle]}/>
+    </SafeAreaView>
   )
 }
 
 export default Lesson1Screen
-
-const styles = StyleSheet.create({})
